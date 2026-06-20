b6f00c8147dcecd3cc9dafdf1ec3dba3a7772ac1
parsa <pinobaxp@gmail.com>
2026-06-20 12:59:41 +0330

    fixed tab inconsistency

--- main.c
+++ main.c
@@ -5,26 +5,26 @@
 #define maxC 12
 
 int printBoard(int board[12][12], int R, int C){
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C; j++) {
-            if(board[i][j] == 1) printf(" X ");
-            else if(board[i][j] == 2) printf(" O ");
-            else printf(" _ ");
-        }
-        printf("\n");
+  for (int i = 0; i < R; i++) {
+    for (int j = 0; j < C; j++) {
+      if(board[i][j] == 1) printf(" X ");
+      else if(board[i][j] == 2) printf(" O ");
+      else printf(" _ ");
     }
-    return 0;
+    printf("\n");
+  }
+  return 0;
 }
 
 int putToken(int board[12][12], int R, int C, int col, int playerNumber){
-    if(col < 0 || col >= C) return -1;
-    for(int i = R - 1; i > -1; i--){
-        if(board[i][col] == 0){
-            board[i][col] = playerNumber;
-            return 0;
-        }
+  if(col < 0 || col >= C) return -1;
+  for(int i = R - 1; i > -1; i--){
+    if(board[i][col] == 0){
+      board[i][col] = playerNumber;
+      return 0;
     }
-    return -2;
+  }
+  return -2;
 }
 
 int main(){  
