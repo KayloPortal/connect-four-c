99f178595ca40f919635b1773b10113c68d919a6
parsa <pinobaxp@gmail.com>
2026-06-20 12:57:27 +0330

    putToken function implementation

--- main.c
+++ main.c
@@ -16,11 +16,30 @@ int printBoard(int board[12][12], int R, int C){
     return 0;
 }
 
+int putToken(int board[12][12], int R, int C, int col, int playerNumber){
+    if(col < 0 || col >= C) return -1;
+    for(int i = R - 1; i > -1; i--){
+        if(board[i][col] == 0){
+            board[i][col] = playerNumber;
+            return 0;
+        }
+    }
+    return -2;
+}
+
 int main(){  
   int board[12][12] = {0};
   int R = 6, C = 7;
   
   printBoard(board, R, C);
+
+  int col;
+  printf("Enter column (0-%d): ", C - 1);
+  scanf("%d", &col);
+  int status = putToken(board, R, C, col, 1);
+  if (status == -1) printf("invalid column. You had to enter a column between 0 and %d.\n", C - 1);
+  else if (status == -2) printf("Column %d is full. You had to choose another column.\n", col);
+  else printBoard(board, R, C);
 
   return 0;
 }
