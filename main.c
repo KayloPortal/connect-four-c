e52ad6b5911998a7a8d3ce1f16f00a2eaf90041d
parsa <pinobaxp@gmail.com>
2026-06-20 14:00:07 +0330

    checkWin function + Complete TUI for HumanVsHuman gamemode

--- main.c
+++ main.c
@@ -27,19 +27,118 @@ int putToken(int board[12][12], int R, int C, int col, int playerNumber){
   return -2;
 }
 
+int checkWin(int board[12][12], int R, int C){
+  // Check horizontal
+  for(int i = 0; i < R; i++){
+    int countFirst = 0, countSecond = 0;
+    for(int j = 0; j < C; j++){
+      if (countFirst == 4) return 1;
+      else if (countSecond == 4) return 2;
+      
+      if (board[i][j] == 1) countFirst++;
+      else countFirst = 0;
+
+      if(board[i][j] == 2) countSecond++;
+      else countSecond = 0;
+    }
+  }
+  // Check vertical
+  for(int j = 0; j < C; j++){
+    int countFirst = 0, countSecond = 0;
+    for(int i = 0; i < R; i++){
+      if (countFirst == 4) return 1;
+      else if (countSecond == 4) return 2;
+
+      if (board[i][j] == 1) countFirst++;
+      else countFirst = 0;
+
+      if(board[i][j] == 2) countSecond++;
+      else countSecond = 0;
+    }
+  }
+  // Check diagonal (top-left to bottom-right)
+  for(int j = 0; j < C - 3; j++){
+    int countFirst = 0, countSecond = 0;
+    for(int i = 0, k = j; i < R && k < C; i++, k++){
+      if (countFirst == 4) return 1;
+      else if (countSecond == 4) return 2;
+
+      if (board[i][k] == 1) countFirst++;
+      else countFirst = 0;
+
+      if(board[i][k] == 2) countSecond++;
+      else countSecond = 0;
+    }
+  }
+  for(int j = C - 1; j > 2; j--){
+    int countFirst = 0, countSecond = 0;
+    for(int i = R - 1, k = j; i >= 0 && k >=0; i--, k--){
+      if (countFirst == 4) return 1;
+      else if (countSecond == 4) return 2;
+
+      if (board[i][k] == 1) countFirst++;
+      else countFirst = 0;
+
+      if(board[i][k] == 2) countSecond++;
+      else countSecond = 0;
+    }
+  }
+  // Check diagonal (bottom-left to top-right)
+  for(int j = C - 1; j > 2; j--){
+    int countFirst = 0, countSecond = 0;
+    for(int i = 0, k = j; i < R && k >= 0; i++, k--){
+      if (countFirst == 4) return 1;
+      else if (countSecond == 4) return 2;
+
+      if (board[i][k] == 1) countFirst++;
+      else countFirst = 0;
+
+      if(board[i][k] == 2) countSecond++;
+      else countSecond = 0;
+    }
+  }
+  for(int i = 0; i < R - 3; i++){
+    int countFirst = 0, countSecond = 0;
+    for(int z = i, j = C - 1; z < R && j >= 0  ; j--, z++){
+      if (countFirst == 4) return 1;
+      else if (countSecond == 4) return 2;
+
+      if (board[z][j] == 1) countFirst++;
+      else countFirst = 0;
+
+      if(board[z][j] == 2) countSecond++;
+      else countSecond = 0;
+    }
+  }
+  return 0;
+}
+
 int main(){  
   int board[12][12] = {0};
   int R = 6, C = 7;
   
   printBoard(board, R, C);
 
-  int col;
-  printf("Enter column (0-%d): ", C - 1);
-  scanf("%d", &col);
-  int status = putToken(board, R, C, col, 1);
-  if (status == -1) printf("invalid column. You had to enter a column between 0 and %d.\n", C - 1);
-  else if (status == -2) printf("Column %d is full. You had to choose another column.\n", col);
-  else printBoard(board, R, C);
-
+  int currentToken = 1;
+  while(1){
+    int col;
+    printf("It's Player %d's turn. Enter column (1-%d): ", currentToken, C);
+    scanf("%d", &col);
+    int status = putToken(board, R, C, --col, currentToken);
+    if (status == -1) printf("invalid column. Enter a column between 1 and %d.\n", C);
+    else if (status == -2) printf("Column %d is full. Enter another column.\n", col);
+    else {
+      printBoard(board, R, C);
+      int win = checkWin(board, R, C);
+      if (win == 1) {
+        printf("Player 1 wins!\n");
+        break;
+      } else if (win == 2) {
+        printf("Player 2 wins!\n");
+        break;
+      }
+      currentToken = currentToken == 1 ? 2 : 1;
+    }
+  }
   return 0;
 }
